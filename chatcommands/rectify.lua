0ea7559e749f1c350820e789048d247b6f9f55e3
flux <25628292+fluxionary@users.noreply.github.com>
2022-10-25 05:45:19 -0700

    allow rectify to point at cardinal directions

--- chatcommands/rectify.lua
+++ chatcommands/rectify.lua
@@ -1,10 +1,34 @@
+local pi = math.pi
+
 minetest.register_chatcommand("rectify", {
 	description = "sets pitch and yaw to (0, 0)",
-	func = function(name)
+	func = function(name, param)
 		local player = minetest.get_player_by_name(name)
 
-		player:set_look_vertical(0)
-		player:set_look_horizontal(0)
+		param = param:trim():lower()
+
+		if param == "e" or param == "east" then
+			player:set_look_vertical(0)
+			player:set_look_horizontal(-pi / 2)
+
+		elseif param == "s" or param == "south" then
+			player:set_look_vertical(0)
+			player:set_look_horizontal(pi)
+
+		elseif param == "w" or param == "west" then
+			player:set_look_vertical(0)
+			player:set_look_horizontal(pi / 2)
+
+		elseif param == "u" or param == "up" then
+			player:set_look_vertical(-pi / 2)
+
+		elseif param == "d" or param == "down" then
+			player:set_look_vertical(pi / 2)
+
+		else
+			player:set_look_vertical(0)
+			player:set_look_horizontal(0)
+		end
 
 		return true
 	end,
