aa55c4bb464e090e4c7652b265c84cb8f8e34f15
flux <25628292+fluxionary@users.noreply.github.com>
2023-03-26 08:24:12 -0700

    instrumentation: settings to instrument mods on load and log every call

--- chatcommands/instrument_mod.lua
+++ chatcommands/instrument_mod.lua
@@ -1,8 +1,12 @@
+local f = string.format
+
 local get_us_time = minetest.get_us_time
 local log = minetest.log
 local log_level = minetest.settings:get("debug_log_level") or "action"
 
 local pairs_by_key = futil.table.pairs_by_key
+
+local s = debuggery.settings
 
 local old_values = {}
 local total_elapsed = {}
@@ -13,10 +17,16 @@ local num_instrumented = 0
 local function instrument(name, value, _cache)
 	if type(value) == "function" then
 		return function(...)
+			if s.instrument_log_every_call then
+				debuggery.log("action", "%s(%s)", name, dump({ ... }))
+			end
 			local begin = get_us_time()
 			local rvs = { value(...) }
 			total_elapsed[name] = (total_elapsed[name] or 0) + get_us_time() - begin
 			total_calls[name] = (total_calls[name] or 0) + 1
+			if s.instrument_log_every_call then
+				debuggery.log("action", "%s(...) -> %s", name, dump(rvs))
+			end
 			return unpack(rvs)
 		end
 	elseif type(value) == "table" then
@@ -31,13 +41,13 @@ local function instrument(name, value, _cache)
 
 		for k, v in pairs(value) do
 			if type(k) == "string" then
-				t[k] = instrument(("%s.%s"):format(name, k), v, _cache)
+				t[k] = instrument(f("%s.%s", name, k), v, _cache)
 			else
-				t[k] = instrument(("%s[%s]"):format(name, k), v, _cache)
+				t[k] = instrument(f("%s[%s]", name, k), v, _cache)
 			end
 		end
 
-		setmetatable(t, instrument(("getmetatable(%s)"):format(name), getmetatable(value), _cache))
+		setmetatable(t, instrument(f("getmetatable(%s)", name), getmetatable(value), _cache))
 
 		return t
 	else
@@ -45,52 +55,83 @@ local function instrument(name, value, _cache)
 	end
 end
 
+local function instrument_mod(mod)
+	debuggery.log("action", "instrumenting %s", mod)
+	old_values[mod] = _G[mod]
+	_G[mod] = instrument(mod, _G[mod])
+	num_instrumented = num_instrumented + 1
+end
+
+local function uninstrument_mod(mod)
+	debuggery.log("action", "uninstrumenting %s", mod)
+	_G[mod] = old_values[mod]
+	old_values[mod] = nil
+	num_instrumented = num_instrumented - 1
+end
+
 minetest.register_chatcommand("instrument_mod", {
 	params = "<global_name>",
 	description = "toggles recording timing data for all functions declared in a particular global",
-	privs = { [debuggery.settings.admin_priv] = true },
+	privs = { [s.admin_priv] = true },
 	func = function(name, param)
+		if param == "" then
+			local mods = {}
+			for mod in pairs(old_values) do
+				table.insert(mods, mod)
+			end
+			if #mods == 0 then
+				return true, "no mods currently instrumented"
+			else
+				return true, "mods currently instrumented: " .. table.concat(mods, ", ")
+			end
+		end
+
 		if not (minetest.global_exists(param) and _G[param]) then
-			return false, ("unknown global %s"):format(param)
+			return false, f("unknown global %s", param)
 		end
 
 		if old_values[param] then
-			_G[param] = old_values[param]
-			old_values[param] = nil
-			num_instrumented = num_instrumented - 1
-			return true, ("instrumentation disabled for %s"):format(param)
+			uninstrument_mod(param)
+			return true, f("instrumentation disabled for %s", param)
 		else
-			old_values[param] = _G[param]
-			_G[param] = instrument(param, _G[param])
-			num_instrumented = num_instrumented + 1
-			return true, ("instrumentation enabled for %s"):format(param)
+			instrument_mod(param)
+			return true, f("instrumentation enabled for %s", param)
 		end
 	end,
 })
 
-local report_interval = debuggery.settings.instrumentation_report_interval
-local elapsed = 0
+local last_call
+futil.register_globalstep({
+	period = s.instrumentation_report_interval,
+	catchup = false,
+	func = function()
+		if num_instrumented == 0 then
+			return
+		end
+		local now = get_us_time()
+		if last_call then
+			local elapsed = now - last_call
+			log(log_level, f("[instrument_mod] in %ss,", elapsed))
+		end
+		last_call = now
 
-minetest.register_globalstep(function(dtime)
-	if num_instrumented == 0 then
-		return
+		for name, num_calls in pairs_by_key(total_calls) do
+			local te = total_elapsed[name]
+
+			log(log_level, f("[instrument_mod] %s was called %s times, used %s us", name, num_calls, te))
+		end
+
+		total_calls = {}
+		total_elapsed = {}
+	end,
+})
+
+minetest.register_on_mods_loaded(function()
+	local mods = s.instrument_on_load:split()
+	for _, mod in ipairs(mods) do
+		if not (minetest.global_exists(mod) and _G[mod]) then
+			error(f("cannot instrument mod %q - it does not exist.", mod))
+		end
+		instrument_mod(mod)
 	end
-
-	elapsed = elapsed + dtime
-	if elapsed < report_interval then
-		return
-	end
-
-	log(log_level, ("[instrument_mod] in %ss,"):format(elapsed))
-	for name, num_calls in pairs_by_key(total_calls) do
-		local te = total_elapsed[name]
-
-		log(log_level, ("[instrument_mod] %s was called %s times, used %s us"):format(name, num_calls, te))
-	end
-
-	total_calls = {}
-	total_elapsed = {}
-
-	elapsed = 0
-	report_interval = debuggery.settings.instrumentation_report_interval
 end)
--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-futil.check_version({ year = 2022, month = 10, day = 24 })
+futil.check_version({ year = 2023, month = 3, day = 24 })
 
 debuggery = fmod.create()
 
