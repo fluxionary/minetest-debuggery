6d522086d07533b6ccfb6e73e1f229501042bac9
flux <25628292+fluxionary@users.noreply.github.com>
2023-08-20 15:14:41 -0700

    remove requirement of admin priv to use distance command

--- chatcommands/distance.lua
+++ chatcommands/distance.lua
@@ -9,7 +9,6 @@ local get_bounds = debuggery.util.get_bounds
 minetest.register_chatcommand("/mark_nodes", {
 	params = S("<limit> <pattern>"),
 	description = S("highlight matching nodes via HUD waypoints"),
-	privs = { [debuggery.settings.admin_priv] = true },
 	func = function(player_name)
 		local pos1, pos2 = get_bounds(player_name)
 		if not (pos1 and pos2) then
