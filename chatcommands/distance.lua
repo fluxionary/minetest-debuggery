a6d595a5a89cfe530d5b1eb9d9a50798255a3408
flux <25628292+fluxionary@users.noreply.github.com>
2023-08-20 17:50:25 -0700

    fix name of command and description

--- chatcommands/distance.lua
+++ chatcommands/distance.lua
@@ -6,9 +6,8 @@ local f = string.format
 local S = debuggery.S
 local get_bounds = debuggery.util.get_bounds
 
-minetest.register_chatcommand("/mark_nodes", {
-	params = S("<limit> <pattern>"),
-	description = S("highlight matching nodes via HUD waypoints"),
+minetest.register_chatcommand("/distance", {
+	description = S("get the distance between two positions (areas or worldedit)"),
 	func = function(player_name)
 		local pos1, pos2 = get_bounds(player_name)
 		if not (pos1 and pos2) then
