19c6df465d9a4ebba8dd381f2f54e0043bfa5d3e
flux <25628292+fluxionary@users.noreply.github.com>
2023-12-19 15:10:51 -0800

    tweak messages

--- chatcommands/whatisthis.lua
+++ chatcommands/whatisthis.lua
@@ -1,11 +1,11 @@
 local S = debuggery.S
 
 minetest.register_chatcommand("whatisthis", {
-	description = S("get the full itemstring of wielded item"),
+	description = S("get the full itemstring of your wielded item"),
 	func = function(player_name)
 		local player = minetest.get_player_by_name(player_name)
 		if not player then
-			return false, S("you are not a real player")
+			return false, S("you must be logged in to use this command.")
 		end
 		return true, player:get_wielded_item():to_string()
 	end,
